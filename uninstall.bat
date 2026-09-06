5fa0b4181b4c298bda93793e13bc2c6569d7e475
Sumon Kayal <57747396+Sumon-Kayal@users.noreply.github.com>
2026-09-07 04:39:10 +0530

    Delete uninstall.bat

--- uninstall.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal
-set "EXE=%LOCALAPPDATA%\FuzzyExit\fuzzy-exit.exe"
-if exist "%EXE%" (
-  "%EXE%" uninstall
-  exit /b %ERRORLEVEL%
-)
-echo Fuzzy Exit is not installed.
-exit /b 0
